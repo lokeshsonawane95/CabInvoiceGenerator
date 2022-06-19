15e99d5868da05f367ecfdf9dd6259b4be60f3cb
lokeshsonawane95 <lokeshsonawane95@gmail.com>
2022-06-19 16:54:57 +0530

    Added to AverageFare

--- CabInvoiceGeneretorMSTest/UnitTest1.cs
+++ CabInvoiceGeneretorMSTest/UnitTest1.cs
@@ -34,5 +34,18 @@ namespace CabInvoiceGeneretorMSTest
 
             Assert.AreEqual(expectedSummary, summary);
         }
+
+        //UC3 Invoice sum
+        [TestMethod]
+        public void GivenMultipleRidesShouldReturnInvoiceSummaryAndAverangeFare()
+        {
+            invoiceGenerator = new InvoiceGenerator(RideType.PREMIUM);
+            Ride[] rides = { new Ride(2.0, 5), new Ride(0.1, 1) };
+
+            InvoiceSummary summary = invoiceGenerator.CalculateFare(rides);
+            InvoiceSummary expectedSummary = new InvoiceSummary(2, 60.0);
+
+            Assert.AreEqual(expectedSummary, summary);
+        }
     }
 }
